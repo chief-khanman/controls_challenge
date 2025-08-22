82181704ae81c243e34311f3337c56844fd3b5ac
chiefkhanman <farhanaadit@gmail.com>
2025-08-22 13:28:20 -0500

    added function description, for update() updated format added new return variable

--- controllers/pid.py
+++ controllers/pid.py
@@ -13,8 +13,17 @@ class Controller(BaseController):
     self.prev_error = 0
 
   def update(self, target_lataccel, current_lataccel, state, future_plan):
+      '''For PID - 
+      Args: 
+        target_lataccel - this information comes from a planner(here its the data, in real world its the planner)
+        current_lataccel
+      Return:
+        control_action'''
       error = (target_lataccel - current_lataccel)
       self.error_integral += error
       error_diff = error - self.prev_error
       self.prev_error = error
-      return self.p * error + self.i * self.error_integral + self.d * error_diff
+      control_action = (self.p * error 
+                      + self.i * self.error_integral 
+                      + self.d * error_diff)
+      return control_action
